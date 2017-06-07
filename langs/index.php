736c8dbe699b72ad3d83e560a59d2da970cac932
BadPixxel <eshop.bpaquier@gmail.com>
2017-06-07 13:48:16 +0200

    Safety Improved - Added index file in all folders Signed-off-by:BadPixxel <eshop.bpaquier@gmail.com>

--- langs/index.php
+++ langs/index.php
@@ -1,7 +1,8 @@
 <?php
-
-/*
+/**
  * This file is part of SplashSync Project.
+ *
+ * Copyright (C) Splash Sync <www.splashsync.com>
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -9,14 +10,14 @@
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
- */
+ **/
 
-header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
-header("Last-Modified: ".gmdate("D, d M Y H:i:s")." GMT");
+header('Expires: Sat, 01 Jan 2000 00:00:00 GMT');
+header('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT');
 
-header("Cache-Control: no-store, no-cache, must-revalidate");
-header("Cache-Control: post-check=0, pre-check=0", false);
-header("Pragma: no-cache");
+header('Cache-Control: no-store, no-cache, must-revalidate');
+header('Cache-Control: post-check=0, pre-check=0', false);
+header('Pragma: no-cache');
 
-header("Location: ../");
+header('Location: ../');
 exit;
