2a1d1b3459fe8fa842e8710ac39c5665803dec74
BadPixxel <eshop.bpaquier@gmail.com>
2018-11-13 19:54:48 +0100

    WIP: Improve Static Code Quality Tools

---
+++ Resources/grumphp/phpstan.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * This file is part of SplashSync Project.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ *  @author    Splash Sync <www.splashsync.com>
+ *  @copyright 2015-2017 Splash Sync
+ *  @license   GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007
+ *
+ **/
+
+/**
+ * Bootstrap Splash for Pḧpstan
+ */
+
+//====================================================================//
+// Include Splash Constants Definitions
+require_once(dirname(dirname(__DIR__)) . "/inc/Splash.Inc.php");
+require_once(dirname(dirname(__DIR__)) . "/inc/defines.inc.php");
+define("SPLASH_SERVER_MODE", 0);
