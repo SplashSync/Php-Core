9ff559141d75588ec7b289503ce8ba8e743dd9fe
BadPixxel <eshop.bpaquier@gmail.com>
2023-09-18 11:35:29 +0200

    FIX - Phpunit on Symfony Bundles

--- Tests/Tools/TestCase.php
+++ Tests/Tools/TestCase.php
@@ -18,21 +18,38 @@
 namespace Splash\Tests\Tools;
 
 use PHPUnit\Framework\TestCase      as BaseTestCase;
+use Splash\Bundle\Tests\WebTestCase;
 use Splash\Client\Splash;
 use Throwable;
 
+/**
+ * Ensure Server Mode is Active
+ */
 if (!defined("SPLASH_SERVER_MODE")) {
     define("SPLASH_SERVER_MODE", true);
 }
 
 /**
- * Base PhpUnit Test Class for Splash Modules Tests
- * May be overridden for Using Splash Core Test in Specific Environments
+ * Check if Php Bundle is installed, if so, use Symfony Web Test Case as base Test Case
  */
-
-if (class_exists("PHPUnit\\Framework\\TestCase")) {
+if (!class_exists("Splash\\Bundle\\Tests\\WebTestCase")) {
     abstract class TestCase extends BaseTestCase
     {
+        /**
+         * {@inheritDoc}
+         */
+        public static function setUpBeforeClass(): void
+        {
+            parent::setUpBeforeClass();
+
+            /**
+             * Ensure Splash definitions are Loaded
+             */
+            if (!defined("SPL_PROTOCOL")) {
+                include dirname(__DIR__, 2)."/inc/Splash.Inc.php";
+            }
+        }
+
         /**
          * @param Throwable $exception
          *
@@ -59,8 +76,28 @@ if (class_exists("PHPUnit\\Framework\\TestCase")) {
             throw $exception;
         }
     }
+
+    /**
+     * Base PhpUnit Test Class for Splash Modules Tests
+     * May be overridden for Using Splash Core Test in Specific Environments
+     */
 } else {
-    abstract class TestCase
+    abstract class TestCase extends WebTestCase
     {
+        /**
+         * {@inheritDoc}
+         */
+        public static function setUpBeforeClass(): void
+        {
+            /** @phpstan-ignore-next-line  */
+            parent::setUpBeforeClass();
+
+            /**
+             * Ensure Splash definitions are Loaded
+             */
+            if (!defined("SPL_PROTOCOL")) {
+                include dirname(__DIR__, 2)."/inc/Splash.Inc.php";
+            }
+        }
     }
 }
