412fbab76ad07464e5fd26c741c5edd68667466e
BadPixxel <eshop.bpaquier@gmail.com>
2025-04-16 11:06:04 +0200

    WIP Migrate to V3 Standard

---
+++ src/Models/Fields/AbstractFieldTemplate.php
@@ -0,0 +1,67 @@
+<?php
+
+/*
+ *  This file is part of SplashSync Project.
+ *
+ *  Copyright (C) Splash Sync  <www.splashsync.com>
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ *  For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code.
+ */
+
+namespace Splash\Core\Models\Fields;
+
+use Splash\Core\Dictionary\Fields\SplFieldProps;
+use Splash\Core\Interfaces\Fields\FieldTemplateInterface;
+
+abstract class AbstractFieldTemplate implements FieldTemplateInterface
+{
+    /**
+     * Template Constructor
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getConfiguration(string $isoLang = null): array
+    {
+        $isoLang ??= self::DF_LANG;
+
+        return array_replace_recursive(
+            $this->getCoreConfiguration($isoLang),
+            array(
+                SplFieldProps::NAME => $this->getName($isoLang),
+                SplFieldProps::DESC => $this->getShortDescription($isoLang),
+            )
+        );
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getMdDescription(string $isoLang = null): string
+    {
+        return $this->getShortDescription($isoLang);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getTechnicalDescription(): string
+    {
+        return $this->getShortDescription();
+    }
+    /**
+     * Get Field Core Configuration to Apply
+     *
+     * @return array<string, array|scalar>
+     */
+    abstract protected function getCoreConfiguration(string $isoLang): array;
+}
